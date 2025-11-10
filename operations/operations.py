2ab2528a4dbeb7a5d4ce6801c6310625dbbeba60
edrewitz <drewitzwx@gmail.com>
2025-11-10 07:19:07 -0800

    Added Mac OS and Linux OS capabilities

--- operations/operations.py
+++ operations/operations.py
@@ -1,17 +1,23 @@
 import ctypes
 import os
 
-# Gets the absolute path to the installation folder to import the C/C++ DLLs
+# Gets the absolute path to the installation folder to import the C/C++ DLLs/SOs
 script_directory = os.path.dirname(os.path.abspath(__file__))
 folder_modified = script_directory.replace("\\", "/")
 
-# Importing the C/C++ DLLs
-cpplibrary = ctypes.CDLL(f"{folder_modified}/sum.dll")
-clibrary = ctypes.CDLL(f"{folder_modified}/difference.dll")
+# Importing the C/C++ DLLs or SOs
+try:
+    # For Windows OS users
+    cpplibrary = ctypes.CDLL(f"{folder_modified}/sum.dll")
+    clibrary = ctypes.CDLL(f"{folder_modified}/difference.dll")
+except Exception as e:
+    # For Mac/Linux OS users
+    cpplibrary = ctypes.CDLL(f"{folder_modified}/sum.so")
+    clibrary = ctypes.CDLL(f"{folder_modified}/difference.so")      
 
 def add(a, b):
     """
-    This function calls a C++ library called 'sum.dll' to
+    This function calls a C++ library to
     add a and b in python at the speed of C++
     """
     
@@ -23,7 +29,7 @@ def add(a, b):
 def subtract(a, b):
     
     """
-    This function calls a C library called 'subtract.dll'
+    This function calls a C library called 
     to subtract a and b in python at the speed of C. 
     """
 
